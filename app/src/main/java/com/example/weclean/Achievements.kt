6185977c4f1fe381f9d7ab5d8dfc9038a7159215
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-02-29 18:05:13 +0100

    Navigation + black screens

---
+++ app/src/main/java/com/example/weclean/Achievements.kt
@@ -0,0 +1,50 @@
+package com.example.weclean
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.weclean.databinding.ActivityAchievementsBinding
+
+class Achievements : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_achievements)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.selectedItemId = R.id.navigation_achievements
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(applicationContext, Home::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_map -> {
+                    startActivity(Intent(applicationContext, Map::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_add -> {
+                    startActivity(Intent(applicationContext, Add::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_achievements -> return@OnNavigationItemSelectedListener true
+                R.id.navigation_profile -> {
+                    startActivity(Intent(applicationContext, Profile::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+
+    }
+}
---
+++ app/src/main/java/com/example/weclean/Profile.kt
@@ -0,0 +1,48 @@
+package com.example.weclean
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_profile)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.selectedItemId = R.id.navigation_profile
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(applicationContext, Home::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_map -> {
+                    startActivity(Intent(applicationContext, Map::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_add -> {
+                    startActivity(Intent(applicationContext, Add::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_achievements -> {
+                    startActivity(Intent(applicationContext, Achievements::class.java))
+                    overridePendingTransition(0,0);
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_profile -> return@OnNavigationItemSelectedListener true
+            }
+            false
+        })
+    }
+}
