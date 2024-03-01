dfd694e762ee6c7f57fb5331ff09bfc5ee675764
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-03-01 15:10:16 +0100

    text input custom view

---
+++ app/src/main/java/com/example/weclean/TextInput.kt
@@ -0,0 +1,44 @@
+package com.example.weclean
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class TextInput : AppCompatEditText {
+    constructor(context: Context) : super(context) {
+        init(null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(attrs, defStyle)
+    }
+
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
+        // Load attributes
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.TextInput, defStyle, 0
+        )
+
+        val inputString = a.getString(
+            R.styleable.TextInput_hintString
+        )
+
+        hint = inputString
+
+
+        a.recycle()
+    }
+}
