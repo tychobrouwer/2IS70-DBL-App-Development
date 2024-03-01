a860d607b9f2078c1ca663f4f4aaaf4f33abd6e7
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-03-01 21:47:33 +0100

    add tags

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/weclean/Add.kt
+++ app/src/main/java/com/example/weclean/Add.kt
@@ -3,13 +3,21 @@ package com.example.weclean
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.marginEnd
+import androidx.core.view.marginTop
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class Add : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +31,45 @@ class Add : AppCompatActivity() {
         communities.add("community 2")
         communities.add("community 3")
 
-        val spinner = findViewById<Spinner>(R.id.select_community)
-        val adapter = ArrayAdapter<String>(
+        val selectCommunitySpinner = findViewById<Spinner>(R.id.select_community)
+        val selectCommunityAdapter = ArrayAdapter<String>(
             this, R.layout.spinner_item, communities)
 
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-        adapter.setNotifyOnChange(true)
-        spinner.adapter = adapter
+        selectCommunityAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        selectCommunityAdapter.setNotifyOnChange(true)
+        selectCommunitySpinner.adapter = selectCommunityAdapter
+
+        val tagButton = findViewById<Button>(R.id.select_tags_button)
+        val tagInput = findViewById<EditText>(R.id.select_tags_input)
+        val tagChipGroup = findViewById<ChipGroup>(R.id.tag_chip_group)
+
+        fun addTagChip(chipText: String) {
+            val chip = Chip(this)
+
+            chip.text = chipText
+            chip.isCloseIconVisible = true
+            chip.chipStrokeWidth = 0F
+            chip.setChipBackgroundColorResource(R.color.blue_500)
+            chip.setTextColor(getColorStateList(R.color.white))
+            chip.closeIconTint = getColorStateList(R.color.white)
+            chip.textSize = 18F
+            chip.setEnsureMinTouchTargetSize(false)
+            chip.setOnCloseIconClickListener {
+                tagChipGroup.removeView(chip)
+            }
+            chip.setOnClickListener {
+                tagChipGroup.removeView(chip)
+            }
+            tagChipGroup.addView(chip)
+        }
+
+        tagButton.setOnClickListener {
+            val tagText = tagInput.text.toString();
+            if (tagText.isNotEmpty()) {
+                addTagChip(tagText)
+                tagInput.setText("")
+            }
+        }
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
         bottomNavigationView.selectedItemId = R.id.navigation_add
