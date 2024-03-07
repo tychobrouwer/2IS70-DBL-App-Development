ae53a96f5f28bd5c8b3c675bf23c132eacedc5cb
Alex-sbol <Aleksandr.s.Bolotskiy@gmail.com>
2024-03-07 19:48:58 +0100

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 3088146b72e96676c86b94c1becb069351a05811, reversing
    changes made to 94a236c85feaa2dc6eaae106e080a747ad8b396e.
    
    reverted merge

--- app/src/main/java/com/example/weclean/CommunityListData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weclean
-
-class CommunityListData(
-    var name: String,
-    var userIsAdmin: Boolean
-)
--- app/src/main/java/com/example/weclean/ProfileCommunities.kt
+++ app/src/main/java/com/example/weclean/ProfileCommunities.kt
@@ -6,62 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 
 class ProfileCommunities : Fragment() {
-    private fun joinCommunityDialog() {
-        val builder = AlertDialog.Builder(activity as AppCompatActivity)
-        builder.setCancelable(true)
-        val joinDialog = builder.create()
-
-        val inflater = layoutInflater
-        val dialogLayout = inflater.inflate(R.layout.profile_join_community, null)
-
-        joinDialog.setView(dialogLayout)
-        joinDialog.show()
-
-        val editText = joinDialog.findViewById<EditText>(R.id.community_code_edittext)
-        val joinButton = joinDialog.findViewById<Button>(R.id.community_join_button)
-
-        joinButton!!.setOnClickListener {
-            val communityCode = editText!!.text
-
-            if (communityCode.isNotEmpty()) {
-                println("Join community with code $communityCode")
-                joinDialog.dismiss()
-            }
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val joinCommunityButton = view.findViewById<Button>(R.id.join_community_button)
         joinCommunityButton.setOnClickListener {
-            println("show join community dialog")
-            joinCommunityDialog()
+            println("join community")
         }
 
         val createCommunityButton = view.findViewById<Button>(R.id.create_community_button)
         createCommunityButton.setOnClickListener {
             println("create community")
         }
-
-        val communities = ArrayList<CommunityListData>()
-        communities.add(CommunityListData("Community 1", true))
-        communities.add(CommunityListData("Community 2", false))
-        communities.add(CommunityListData("Community 3", false))
-
-        val communitiesListAdapter = CommunityListAdapter(
-            activity as AppCompatActivity,
-            communities
-        )
-
-        val communitiesListView  = view.findViewById<ListView>(R.id.community_list)
-        communitiesListView.adapter = communitiesListAdapter
     }
 
     override fun onCreateView(
