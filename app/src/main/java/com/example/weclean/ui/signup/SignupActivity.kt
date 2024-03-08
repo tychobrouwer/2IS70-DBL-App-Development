0981ea52e7702c2aa04e995795b5066431912154
JoppeGijsen <j.gijsen@student.tue.nl>
2024-03-08 21:30:15 +0100

    Refactored sign up activity to its own repository

--- app/src/main/java/com/example/weclean/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/weclean/ui/signup/SignupActivity.kt
@@ -1,10 +1,11 @@
-package com.example.weclean
+package com.example.weclean.ui.signup
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weclean.R
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
