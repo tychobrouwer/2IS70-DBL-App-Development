2f6ed4d226a9bf976f98239425c3b4a60f4b68ed
JoppeGijsen <j.gijsen@student.tue.nl>
2024-03-08 17:09:20 +0100

    Added Sign up screen front end

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.maps.android:android-maps-utils:2.2.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
