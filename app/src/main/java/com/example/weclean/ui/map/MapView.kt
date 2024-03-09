d61cb1f654caf3b99aef34c8535701ae35577aa3
Tycho Brouwer <tychobrouwer33@gmail.com>
2024-03-09 23:57:55 +0100

    map get and set location + set map style

--- app/src/main/java/com/example/weclean/ui/map/MapView.kt
+++ app/src/main/java/com/example/weclean/ui/map/MapView.kt
@@ -1,15 +1,90 @@
 package com.example.weclean.ui.map
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.weclean.R
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MapView : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var permissionCode = 101
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        getCurrentLocation()
+    }
+
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                activity as AppCompatActivity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                activity as AppCompatActivity,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+            ActivityCompat.requestPermissions(
+                activity as AppCompatActivity,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionCode
+            )
+
+            return
+        }
+
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(activity as AppCompatActivity)
+
+        val mapFragment = getChildFragmentManager().findFragmentById(R.id.map_fragment) as SupportMapFragment
+
+        fusedLocationProviderClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token)
+            .addOnSuccessListener { location: Location? ->
+                if (location == null)
+                    println("Couldn't get location")
+                else {
+                    mapFragment.getMapAsync { googleMap ->
+                        val latLng = LatLng(location.latitude, location.longitude)
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                        googleMap.setIndoorEnabled(false)
+                        googleMap.setMapStyle(
+                            MapStyleOptions.loadRawResourceStyle(
+                                activity as AppCompatActivity,
+                                R.raw.mapstyle))
+                    }
+                }
+            }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            permissionCode -> if (
+                grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                getCurrentLocation()
+            }
+        }
     }
 
     override fun onCreateView(
