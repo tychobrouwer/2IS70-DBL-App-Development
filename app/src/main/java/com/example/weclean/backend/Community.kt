4ff8c3837c38e13b257f3a8835ad2ccb1ce08240
Aseem <shakir.aseem@gmail.com>
2024-03-13 15:12:45 +0100

    deleted community data class

--- app/src/main/java/com/example/weclean/backend/Community.kt
+++
@@ -1,7 +0,0 @@
-package com.example.weclean.backend
-
-data class Community(
-    var communityId: String,
-    var communityMembers: ArrayList<User>,
-    //var cleanUpEvents: ArrayList<CleanUpEvent>
-)
