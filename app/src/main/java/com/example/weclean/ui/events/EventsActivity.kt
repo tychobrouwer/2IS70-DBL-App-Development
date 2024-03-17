8a207f04d92eeb8942ee6359eb7b17023d68145c
JoppeGijsen <j.gijsen@student.tue.nl>
2024-03-17 14:10:36 +0100

    Added UI for upcoming events (I didn't do it with the fragments, since that was very vague imo, also the disign is not final since it should be redesignt as the guy said)

---
+++ app/src/main/java/com/example/weclean/ui/events/EventsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.weclean.ui.events
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.weclean.R
+
+class EventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_events)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
