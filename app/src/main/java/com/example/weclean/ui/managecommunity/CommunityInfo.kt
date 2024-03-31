910194bc89181f090c135263c369578f8c44ff57
Aseem <shakir.aseem@gmail.com>
2024-03-31 16:40:09 +0200

    kotlin files for manage community activity and fragments

---
+++ app/src/main/java/com/example/weclean/ui/managecommunity/CommunityInfo.kt
@@ -0,0 +1,28 @@
+package com.example.weclean.ui.managecommunity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weclean.R
+
+class CommunityInfo : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_community_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // TODO Setting up UI elements and handling button clicks
+        //val editButton = view.findViewById<Button>(R.id.edit_button)
+        //  editButton.setOnClickListener {
+        // Handle edit button click
+        //}
+    }
+}
